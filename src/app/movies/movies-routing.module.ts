68645a5dafecfb073267330b7a43691bb74708ad
RodrigoVellozo <rodrigovellozosi@gmail.com>
2023-09-20 11:27:31 -0300

    ajustar rotas

--- src/app/movies/movies-routing.module.ts
+++ src/app/movies/movies-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MoviesListComponent } from './movies-list/movies-list.component';
+import { MoviesDetailsComponent } from './movies-details/movies-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MoviesListComponent
+  },
+  {
+    path: 'movie/:imdbID', component: MoviesDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesListComponent } from './movies-list/movies-list.component';
+import { MoviesDetailsComponent } from './movies-details/movies-details.component';
 
 
 @NgModule({
   declarations: [
-    MoviesListComponent
+    MoviesListComponent,
+    MoviesDetailsComponent
   ],
   imports: [
     CommonModule,
     MoviesRoutingModule
+  ],
+  exports: [
+    MoviesListComponent,
+    MoviesDetailsComponent
   ]
 })
 export class MoviesModule { }
