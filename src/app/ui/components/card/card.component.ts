ae3f4a7434232a3b020c4fce76f72861f4ab93ff
RodrigoVellozo <rodrigovellozosi@gmail.com>
2023-09-21 13:44:31 -0300

    adicionar tooltip

--- src/app/ui/components/card/card.component.ts
+++ src/app/ui/components/card/card.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import {TooltipPosition, MatTooltipModule} from '@angular/material/tooltip';
 import { Router, RouterModule } from '@angular/router';
 import { Movie } from 'src/app/core/models/movie';
 import { ConvertMinToHoursPipe } from '../../pipes/convert-min-to-hours.pipe';
@@ -13,11 +14,12 @@ import { EllipsisPipe } from '../../pipes/ellipsis.pipe';
   imports: [
     CommonModule,
     ConvertMinToHoursPipe,
+    RouterModule,
+    EllipsisPipe,
+    MatChipsModule,
     MatCardModule,
     MatButtonModule,
-    MatChipsModule,
-    RouterModule,
-    EllipsisPipe
+    MatTooltipModule
   ],
   selector: 'app-card',
   templateUrl: './card.component.html',
